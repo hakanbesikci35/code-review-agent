45387d0421aea1de32c13ddd8a6c30ff2b116113
hakanbesikci35 <hakan.besikci01@gmail.com>
2026-05-21 17:48:52 +0300

    load.env degisti

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 import uvicorn
 from io import BytesIO
 
